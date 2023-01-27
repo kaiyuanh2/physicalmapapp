a567839aabee8dc2f3b31e6782c5cd1e76e3a6f1
Kaiyuan Hu <rail_hu@MacBook-Pro-M2.local>
2023-01-27 14:33:48 -0800

    module split

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
+app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'))
+
+app.engine('ejs', ejsMate)
 
 app.get('/', (req, res) => {
     res.render('index')
