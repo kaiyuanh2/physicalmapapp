bdc7693846a4d445a75811604dbf5790f880b51b
Kaiyuan Hu <52292794+kaiyuanh2@users.noreply.github.com>
2023-05-21 01:21:05 -0700

    parsed all school districts

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/ExpressError');
 const flash = require('connect-flash');
-const {validateParameters, validateParametersPost} = require('./middleware');
+const fs = require('fs');
+const { validateParameters, validateParametersPost } = require('./middleware');
 const session = require('express-session');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use('/node_modules', express.static(__dirname + '/node_modules/'));
 
@@ -33,6 +34,18 @@ app.use(session(sessionConfig));
 app.use(flash());
 
 app.get('/', (req, res) => {
+    fs.readFile('./public/entities.json', 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err);
+            return;
+        }
+        try {
+            const map = new Map(Object.entries(JSON.parse(jsonString)));
+            console.log(map);
+        } catch (err) {
+            console.log('Error parsing JSON string:', err);
+        }
+    });
     res.render('index')
 })
 
@@ -40,7 +53,7 @@ app.get('/map', (req, res) => {
     var item = req.query.item;
     var grade = req.query.grade;
     var year = req.query.year;
-    res.render('map', {item, grade, year});
+    res.render('map', { item, grade, year });
 })
 
 app.post('/map', (req, res) => {
@@ -48,15 +61,15 @@ app.post('/map', (req, res) => {
     var item = req.body.item;
     var grade = req.body.grade;
     var year = req.body.year;
-    res.render('map', {item, grade, year});
+    res.render('map', { item, grade, year });
 })
 
 app.get('/california', validateParameters, (req, res) => {
     var item = '';
     var grade = '';
     var year = '';
-    var checkboxStr = '';
-    res.render('california', {item, grade, year, checkboxStr, messages: req.flash('error')});
+    var checkboxStr = 'all';
+    res.render('california', { item, grade, year, checkboxStr, messages: req.flash('error') });
 })
 
 app.post('/california', validateParametersPost, (req, res) => {
@@ -65,7 +78,7 @@ app.post('/california', validateParametersPost, (req, res) => {
     var grade = req.body.grade;
     var year = req.body.year;
     var checkboxStr = req.body.checkboxStr;
-    res.render('california', {item, grade, year, checkboxStr, messages: req.flash('error')});
+    res.render('california', { item, grade, year, checkboxStr, messages: req.flash('error') });
 })
 
 app.all('*', (req, res, next) => {
@@ -73,8 +86,8 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;	
-    if (!err.message) err.message = 'Error!'	
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Error!'
     res.status(statusCode).render('error', { err })
 })
 
--- public/parseform.js
+++ public/parseform.js
@@ -2,19 +2,19 @@ var form = document.getElementById('display_options');
 form.addEventListener("submit", submitFunction, false);
 
 function submitFunction(event) {
-    event.preventDefault();
-    var boxes = document.getElementsByClassName('form-check-input');
-    var checked = [];
-    for(var i=0; boxes[i]; ++i){
-      if(boxes[i].checked){
-        checked.push(boxes[i].name);
-      }
+  event.preventDefault();
+  var boxes = document.getElementsByClassName('form-check-input');
+  var checked = [];
+  for (var i = 0; boxes[i]; ++i) {
+    if (boxes[i].checked) {
+      checked.push(boxes[i].name);
     }
+  }
 
-    var checkedStr = checked.join(",");
+  var checkedStr = checked.join(",");
 
-    document.getElementById('checkbox_str').value = checkedStr;
-    form.submit();
+  document.getElementById('checkbox_str').value = checkedStr;
+  form.submit();
 
-    return false;
+  return false;
 }
