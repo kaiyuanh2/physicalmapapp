a5cf576f6b5b2295fcee7abd797aabaea8c0766f
Kaiyuan Hu <52292794+kaiyuanh2@users.noreply.github.com>
2024-04-07 23:01:04 -0700

    add cors

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ let mapboxToken = '';
 getSecret('mapbox').then(secret => {
     mapboxToken = secret;
 });
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({limit: '10mb', extended: true, parameterLimit: 10000}));
