f2bdb905595cedb2ef78cc4153c18c9cc860dc00
Kaiyuan Hu <52292794+kaiyuanh2@users.noreply.github.com>
2024-04-07 22:50:39 -0700

    Update mapbox_setup.js

--- mapbox_setup.js
+++ mapbox_setup.js
@@ -1,16 +1,26 @@
-const AWS = require('aws-sdk');
-const secrets = new AWS.SecretsManager();
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
 
 async function getSecret(secret_name) {
+    const secret_name = "mapbox";
+    const client = new SecretsManagerClient({
+        region: "us-west-1",
+    });
+    let response;
+
     try {
-        const response = await secrets.getSecretValue({SecretId: secret_name}).promise();
-        let secret = response.SecretString;
-        return secret;
+        response = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secret_name,
+            VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+        }));
     } catch (err) {
         // For a list of exceptions thrown, see
         // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         throw err;
     }
+
+    let secret = response.SecretString;
+    return secret;
 }
 
 async function getSecretDev(secret_name) {
